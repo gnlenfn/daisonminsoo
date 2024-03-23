17a97e37b4ec9ab5e62bf3445eb42aa356e0933a
gnlenfn <akenfn513@gmail.com>
2024-03-23 23:20:03 +0900

    chore: remove unused import

--- src/main/java/com/potential/hackathon/email/EmailService.java
+++ src/main/java/com/potential/hackathon/email/EmailService.java
@@ -1,14 +1,10 @@
 package com.potential.hackathon.email;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Slf4j
