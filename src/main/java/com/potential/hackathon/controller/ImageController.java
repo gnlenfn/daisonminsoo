3217bac8e2759ea5b8c8b61bbc0ed98a2d5029e6
gnlenfn <akenfn513@gmail.com>
2024-03-29 20:02:03 +0900

    chore: fix rest controller

--- src/main/java/com/potential/hackathon/controller/ImageController.java
+++ src/main/java/com/potential/hackathon/controller/ImageController.java
@@ -6,13 +6,12 @@ import com.potential.hackathon.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/images")
 @RequiredArgsConstructor
 public class ImageController {
