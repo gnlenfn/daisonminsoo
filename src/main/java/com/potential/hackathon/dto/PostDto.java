26eaec0eb14696d19d512c363eda0b139d0a7263
gnlenfn <akenfn513@gmail.com>
2024-03-29 10:41:47 +0900

    feature: swagger docs

--- src/main/java/com/potential/hackathon/dto/PostDto.java
+++ src/main/java/com/potential/hackathon/dto/PostDto.java
@@ -1,6 +1,7 @@
 package com.potential.hackathon.dto;
 
 import com.potential.hackathon.entity.Images;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,13 +13,22 @@ import java.util.List;
 public class PostDto {
 
     private Long id;
+
     @NotEmpty
+    @Schema(description = "게시글 제목", example = "제목입니다")
     private String title;
+
     @NotEmpty
+    @Schema(description = "게시글 내용", example = "this is contents of posting")
     private String content;
+
     private List<Images> images;
+
     @NotEmpty
+    @Schema(description = "게시글 수정/삭제를 위한 비밀번호", example = "123456")
     private String password;
+
     @NotEmpty
+    @Schema(description = "작성자 닉네임", example = "이거모야")
     private String userId;
 }
