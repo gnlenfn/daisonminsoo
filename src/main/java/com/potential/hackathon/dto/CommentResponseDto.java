63857ece0590a6bb9476b3a2b120b35603dbd508
gnlenfn <akenfn513@gmail.com>
2024-03-30 21:07:04 +0900

    feat: add user info in comment response

--- src/main/java/com/potential/hackathon/dto/CommentResponseDto.java
+++ src/main/java/com/potential/hackathon/dto/CommentResponseDto.java
@@ -19,17 +19,17 @@ public class CommentResponseDto {
 
     private Long id;
     private String content;
-    private UUID userId;
+    private UserResponseDto user;
     private List<CommentChildResponseDto> children;
     private LocalDateTime createdAt;
 
     public static CommentResponseDto findFromComment(Comments comment) {
         return comment.getIsDeleted() ?
-                new CommentResponseDto(comment.getId(), "삭제된 댓글입니다", comment.getUsers().getId(), null, comment.getCreatedAt()) :
+                new CommentResponseDto(comment.getId(), "삭제된 댓글입니다", UserResponseDto.findFromUsers(comment.getUsers()), null, comment.getCreatedAt()) :
                 new CommentResponseDto(
                         comment.getId(),
                         comment.getContent(),
-                        comment.getUsers().getId(),
+                        UserResponseDto.findFromUsers(comment.getUsers()),
                         comment.getChildren().stream().map(CommentChildResponseDto::findFromComment).collect(Collectors.toList()),
                         comment.getCreatedAt()
         );
